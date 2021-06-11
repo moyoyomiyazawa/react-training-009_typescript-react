581707a9003381b5b467c28fccb4274dccc135f2
Yuya Kasai <moyoyomiyazawa@gmail.com>
2021-06-11 11:03:59 +0900

    state, response, propsの型定義

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
+import { Todo } from './Todo';
+
+// 型エイリアスを定義
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 function App() {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  const onClickFetchData = () => {
+    axios
+      .get<TodoType[]>('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => {
+        res.data.map((todo) => todo.title);
+        setTodos(res.data);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
     </div>
   );
 }
